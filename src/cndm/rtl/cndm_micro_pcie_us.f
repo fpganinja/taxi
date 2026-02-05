20a5166f9672324fb771248395daccd08d204f67
Alex Forencich <alex@alexforencich.com>
2026-02-05 14:37:27 -0800

    cndm: Add PCIe configuration shim instance to read extended tag enable bit
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- src/cndm/rtl/cndm_micro_pcie_us.f
+++ src/cndm/rtl/cndm_micro_pcie_us.f
@@ -2,4 +2,5 @@ cndm_micro_pcie_us.sv
 cndm_micro_core.f
 ../lib/taxi/src/pcie/rtl/taxi_pcie_us_axil_master.sv
 ../lib/taxi/src/pcie/rtl/taxi_pcie_us_msi.sv
+../lib/taxi/src/pcie/rtl/taxi_pcie_us_cfg.sv
 ../lib/taxi/src/dma/rtl/taxi_dma_if_pcie_us.f

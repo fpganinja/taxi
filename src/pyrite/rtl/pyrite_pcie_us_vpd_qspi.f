d627de2c1501481368f7c86fae73534a7b3ddf64
Alex Forencich <alex@alexforencich.com>
2026-02-24 11:47:59 -0800

    pyrite: Ensure APB IF is included
    Signed-off-by: Alex Forencich <alex@alexforencich.com>

--- src/pyrite/rtl/pyrite_pcie_us_vpd_qspi.f
+++ src/pyrite/rtl/pyrite_pcie_us_vpd_qspi.f
@@ -2,3 +2,4 @@ pyrite_pcie_us_vpd_qspi.sv
 ../lib/taxi/src/pcie/rtl/taxi_pcie_us_vpd.sv
 ../lib/taxi/src/apb/rtl/taxi_apb_ram.sv
 ../lib/taxi/src/apb/rtl/taxi_apb_interconnect.sv
+../lib/taxi/src/apb/rtl/taxi_apb_if.sv
